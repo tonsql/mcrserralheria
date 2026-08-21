4628559ffb92beccdfd4cc16bf245e5fb6736922
ton <eotomdz7@gmail.com>
2026-08-21 09:46:04 -0300

    Delete js directory

--- js/supabase-config.js
+++
@@ -1,5 +0,0 @@
-/* Configuração pública do Supabase. Nunca coloque secret/service_role aqui. */
-window.MCR_SUPABASE = {
-  url: 'https://ipglkwryabspwrbrezlx.supabase.co',
-  anonKey: 'sb_publishable_T6FJksZ09t0trG9GCIO72A_k7qs1Ru2'
-};
